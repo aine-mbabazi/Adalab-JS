2d4f5f39cfca6551808ed6eb8572c90c335d9caa
aine-mbabazi <patainembabazi@gmail.com>
2024-02-28 19:50:45 +0300

    Added different Variables

---
+++ index.js
@@ -0,0 +1,22 @@
+var num = 1;
+var num = 2;
+console.log({ num });
+
+let a = 2;
+a = 5;
+console.log(a);
+
+const person = "John Doe";
+console.log({ person });
+
+let p = 8;
+let P = 7;
+console.log("small p", p);
+console.log("capital p", P);
+
+var dateed = new Date();
+console.log(dateed);
+
+var dat = new Date();
+console.log(dat)
+
